8328952cf38388531bc259ec4584f0d385d42e34
gabrielld06 <gabriellimadias2001@gmail.com>
2020-10-30 20:41:24 -0300

    Dobrando Papeç

---
+++ 3105.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int x, y, n, m, k, g_atual, g_menor = 2147483647;
+	scanf("%d %d %d", &n, &m, &k);
+	for(x=0.0;x<k;x+=1) {
+		for(y=0.0;(x+1)*(y+1) <= k;y+=1) {
+			if(n/(x+1) > m/(y+1)) {
+				g_atual = n/(x+1);
+			} else {
+				g_atual = m/(y+1);
+			}
+			if (g_atual < g_menor) 
+				g_menor = g_atual;
+		}
+	}
+	printf("%d\n", g_menor);
+	return 0;
+}
---
+++ 3105.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+	int x, y;
+	double n, m, k, g_atual, g_menor = INFINITY;
+	cin >> n >> m >> k;
+	for(x=0;x<k;x++) {
+		for(y=0;(x+1)*(y+1) <= k;y++) {
+			if(n/(x+1) > m/(y+1)) {
+				g_atual = n/(x+1);
+			} else {
+				g_atual = m/(y+1);
+			}
+			if (g_atual < g_menor) 
+				g_menor = g_atual;
+		}
+	}
+	cout << fixed;
+	cout << setprecision(0) << g_menor << "\n";
+	return 0;
+}
