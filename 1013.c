d662ae4bdeb21acb6c46514283359b4cfd7a82be
gabrielld06 <gabriellimadias2001@gmail.com>
2020-10-30 20:18:11 -0300

    O Maior

---
+++ 1013.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int a, b, c, MaiorAB, Maior;
+	scanf("%d %d %d", &a, &b, &c);
+	MaiorAB = (a+b+abs(a-b))/2;
+	Maior = (MaiorAB+c+abs(MaiorAB-c))/2;
+	printf("%d eh o maior\n", Maior);
+	return 0;
+}
