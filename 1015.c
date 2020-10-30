577dec0a0f2f766ccbd724b9a1cd63e9677fa3b1
gabrielld06 <gabriellimadias2001@gmail.com>
2020-10-30 20:18:37 -0300

    Distância Entre Dois Pontos

---
+++ 1015.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	double x1, x2, y1, y2, d;
+	scanf("%lf %lf %lf %lf", &x1, &y1, &x2, &y2);
+	d = pow((x2-x1), 2) + pow((y2-y1), 2);
+	printf("%.4lf\n", sqrt(d));
+	return 0;
+}
