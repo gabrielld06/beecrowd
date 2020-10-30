c54b7080fd6bc0c991e665530b2dbe5939c9bd5a
gabrielld06 <gabriellimadias2001@gmail.com>
2020-10-30 20:13:17 -0300

    Hello World!

---
+++ 1000.c
@@ -0,0 +1,72 @@
+############################# C, C99
+#include <stdio.h>
+ 
+int main() {
+ 
+    printf("Hello World!\n");
+ 
+    return 0;
+}
+
+############################# Python
+
+# -*- coding: utf-8 -*-
+
+print("Hello World!")
+
+
+############################# C#
+
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        Console.WriteLine("Hello World!");
+
+    }
+
+}
+
+############################# Java, Java14
+
+import java.io.IOException;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        System.out.println("Hello World!");
+ 
+    }
+ 
+}
+
+############################# C++, C++17
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    cout << "Hello World!\n";
+ 
+    return 0;
+}
+
+############################# Pascal
+
+Program Pzim ;
+Begin
+   writeln('Hello World!');
+End.
+
+#############################
