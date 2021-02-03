86361dd83dc5bbfec683f86fc787c99bae05ef1f
gabrielld06 <gabriellimadias2001@gmail.com>
2021-02-03 20:26:29 -0300

    Fibonacci, Quantas Chamadas?

--- 1029.cpp
+++ 1029.cpp
@@ -2,11 +2,44 @@
 
 using namespace std;
 
-int fib(int n, int *num) {
+/* int fib(int n, int *num) {
 	*num+=1;
-	if(n==0) return 0;
-	else if(n==1) return 1;
-	else return fib(n-1, num) + fib(n-2, num);
+	if(n==0) 
+		return 0;
+	else if(n==1) 
+		return 1;
+	else 
+		return fib(n-1, num) + fib(n-2, num);
+} */
+
+int memo[40], memocall[40];
+
+int chamadas(int n) {
+	int ans;
+	if(memocall[n] == -1) {
+		if(n==0) 
+			ans = 1;
+		else if(n==1) 
+			ans = 1;
+		else 
+			ans = 1 + chamadas(n-1) + chamadas(n-2);
+		memocall[n] = ans;
+	}
+	return memocall[n];
+}
+
+int fib(int n) {
+	int ans;
+	if(memo[n]==-1) {
+		if(n==0) 
+			ans = 0;
+		else if(n==1) 
+			ans = 1;
+		else 
+			ans = fib(n-1) + fib(n-2);
+		memo[n] = ans;
+	}
+	return memo[n];
 }
 
 int main() {
@@ -14,8 +47,13 @@ int main() {
 	cin >> n;
 	for(int i=0;i<n;i++) {
 		cin >> f;
-		num = 0;
-		res = fib(f, &num);
-		cout << "fib(" << f << ") = " << num-1 << " calls = " << res << endl; 
+		for(int i=0;i<=f;i++) {
+			memo[i] = -1;
+			memocall[i] = -1;
+		}
+		//num = 0;
+		//res = fib(f, &num);
+		//cout << "fib(" << f << ") = " << num-1 << " calls = " << res << endl; 
+		cout << "fib(" << f << ") = " << chamadas(f)-1 << " calls = " << fib(f) << endl; 
 	}
 }
